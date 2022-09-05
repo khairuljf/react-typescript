bfd5a482ffe4ed683caab9e9cdef37d57cc01413
khairul islam <khairuljf@gmail.com>
2022-09-05 08:09:09 +0600

    At last form hander done with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './component/InputField';
-import TaskList from './component/TaskList';
+import TodoList from './component/TodoList';
+import uuid from 'react-uuid';
 
 
 
 const App:React.FC = () => {
 
-  const [todo, setTodo] = useState< string>("")
+  interface TodosType{
+    id:string;
+    todo:string;
+    isDone:boolean;
+  }
+
+  const [todo, setTodo] = useState< string>("");
+  const [todos, setTodos] = useState<TodosType[]>([])
+
+
   
+  const handleSubmit = (e: React.FormEvent) =>{
+      e.preventDefault();
+      setTodos([...todos, {id:uuid(), todo, isDone:false}])
+
+      setTodo("");
+  }
+
+
   
+ 
   return (
     <div className="App">
       <span className='heading'> Tasks  </span>
-
-      <InputField todo={todo}  setTodo={setTodo} />
-      <TaskList />
+      <InputField todo={todo}  setTodo={setTodo} handleSubmit={handleSubmit}  />
+      <TodoList />
     </div>
   );
 }
--- src/component/InputField.tsx
+++ src/component/InputField.tsx
@@ -3,13 +3,21 @@ import "./style.css";
 
 interface Props{
     todo:string;
-    setTodo:React.Dispatch<React.SetStateAction<string>>
+    setTodo:React.Dispatch<React.SetStateAction<string>>;
+    handleSubmit:React.FormEventHandler;
 };
 
-const InputField:React.FC<Props> = ({todo, setTodo}) => {
+const InputField:React.FC<Props> = ({todo, setTodo, handleSubmit}) => {
   return (
-    <form className='input'>
-        <input type="input" placeholder='Enter task name'  className='input__box' />
+    <form className='input' onSubmit={handleSubmit}>
+        <input
+           type="input"
+            placeholder='Enter task name'  
+            className='input__box'
+            value={todo}
+            onChange={(e)=>setTodo(e.target.value)}
+
+        />
         <button className='input__submit' type='submit'>Go</button>
     </form>
   )
